769abab3c5cecd795864b93f354d271ad18a60b6
Sergey Perekatov <perekatovsergey@yandex.ru>
2019-04-07 17:39:54 +0200

    Create new model contacts

--- db/migrate/20190407151942_create_contacts.rb
+++ db/migrate/20190407151942_create_contacts.rb
@@ -1,7 +1,7 @@
 class CreateContacts < ActiveRecord::Migration[5.2]
   def change
     create_table :contacts do |t|
-      t.string :name
+      t.text :message
       t.string :email
 
       t.timestamps
