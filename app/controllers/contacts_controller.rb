87d612a621064ec55b7ca9471045fddbc18ed291
Sergey Perekatov <perekatovsergey@yandex.ru>
2019-04-07 18:10:44 +0200

    Save contat on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,17 @@
 class ContactsController < ApplicationController
 
-def new
+	def new
+	end
+
+	def create
+		@contact = Contact.new(contact_params)
+		@contact.save
+	end
+
+private
+
+	def contact_params
+		params.require(:contact).permit(:email, :message)
+	end
 end
 
-def create
-end
-
-end
