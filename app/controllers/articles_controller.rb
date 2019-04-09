5d0feccdfcd00148e06fc7c3e14ba0c801ef11b5
Sergey Perekatov <perekatovsergey@yandex.ru>
2019-04-09 21:42:19 +0200

    implement articles#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+	def show 
+		@article = Article.find(params[:id])
+	end
+
 	def new
 	
 	end	
@@ -9,6 +13,7 @@ class ArticlesController < ApplicationController
 		
 		if @article.valid?
 			@article.save
+			redirect_to @article
 		else
 			render action:'new' # method render возврат new 
 		end
