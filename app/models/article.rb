6c3ff57e474c98dd39a4799e0ca9b607944471c3
Sergey Perekatov <perekatovsergey@yandex.ru>
2019-04-20 15:45:31 +0200

    define dependent: :delete_all in Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,13 @@
 class Article < ApplicationRecord
-	has_many :comments
+	validates :title, presence: true
+  validates :text, presence: true
+ has_many :comments, dependent: :delete_all
+
+  def subject
+    title
+  end
+
+  def last_comment
+    comments.last
+  end
 end
