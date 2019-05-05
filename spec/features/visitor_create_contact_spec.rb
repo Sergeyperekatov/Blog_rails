5b08718a2ca3e060a6b51ac3de98a3a3fab0d68b
Sergey Perekatov <perekatovsergey@yandex.ru>
2019-05-05 23:59:55 +0300

    Create new tests

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -12,6 +12,9 @@ scenario "allows a guest to create contact"
     visit '/contacts'
     fill_in :contact_email, :with => 'user@example.ru'
     fill_in :contact_message, :with => 'something'
+   
     click_button 'send message'
+
+    expect(page).to have_content 'Thanks!'
   end
 end
