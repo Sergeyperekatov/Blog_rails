75914bd2f1e1fe4ede7a60df9d9713a78186c3f4
Sergey Perekatov <perekatovsergey@yandex.ru>
2019-05-20 01:37:59 +0300

    Finally setting date of publication comment

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,9 @@
-encoding: cp866
 # Load the Rails application.
 require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-Time::DATE_FORMATS[:ru_datetime] = "%Y.%m.%d в %k:%M:%S"
+
+#encoding: utf-8
+
+Time::DATE_FORMATS[:ru_datetime] = '%B %Y'
