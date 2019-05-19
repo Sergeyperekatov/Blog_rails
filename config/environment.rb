dfe54bf1b4a2cb816da807db2691353f54b94b09
Sergey Perekatov <perekatovsergey@yandex.ru>
2019-05-19 16:33:19 +0300

    Add date od create comment

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
+encoding: cp866
 # Load the Rails application.
 require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Time::DATE_FORMATS[:ru_datetime] = "%Y.%m.%d в %k:%M:%S"
